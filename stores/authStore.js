0fe87b60a850279f0e4a63b0a473534c0b747b44
Alkandari-Y <yayalkandari@gmail.com>
2021-11-21 22:11:52 +0300

    home screen file and basic nav with authStore

--- stores/authStore.js
+++ stores/authStore.js
@@ -11,8 +11,52 @@ class AuthStore {
 
 	user = null;
 
+    setUser = async (token) => {
+        const userToken = JSON.stringify(token)
+		await AsyncStorage.setItem("myToken", userToken);
+		instance.defaults.headers.common.Authorization = `Bearer ${token}`;
+		this.user = decode(token);
+	};
+
+	checkForToken = async() => {
+		const token = await AsyncStorage.getItem("myToken");
+		if (token) {
+			const tempUser = decode(token);
+			const time = tempUser.exp;
+			if (time > Date.now()) {
+				this.setUser(token);
+			} else {
+				this.signOut();
+			}
+		}
+	};
+
+	signIn = async (user ) => {
+		try {
+			const res = await instance.post("/login", user);
+            await this.setUser(res.data.token);
+		} catch (error) {
+            console.log(error);
+		}
+	};
+
+	signUp = async (user) => {
+		try {
+			const res = await instance.post("/signup", user);
+            await this.setUser(res.data.token);
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	signOut = async () => {
+		delete instance.defaults.headers.common.Authorization;
+		await AsyncStorage.removeItem("myToken");
+        this.user = null;
+        console.log(`logged out`)
+	};
 }
 
 const authStore = new AuthStore();
-
+authStore.checkForToken();
 export default authStore;
