ab6ef797d648d3fb27a02a1cf65d45938c9ad1de
hassan alhassaini <hassandak1d@hotmail.com>
2021-11-24 15:11:50 +0300

    done delete and edit trips

--- components/TripList/TripItem.js
+++ components/TripList/TripItem.js
@@ -1,20 +1,11 @@
 import React from "react";
-import { Text } from "react-native";
-// Fe-CRUDS
-import { Button} from "native-base";
+import { Pressable, Text } from "react-native";
 
-import { Center, Container, HStack, Image, Pressable, View } from "native-base";
+import { HStack, Image, View } from "native-base";
 
 import { baseUrl } from "../../stores/baseUrl";
-import tripStore from "../../stores/tripStore";
-import { observer } from "mobx-react";
-import authStore from "../../stores/authStore";
 
 const TripItem = ({ trip, navigation }) => {
-  const handleDelete = () => {
-    tripStore.deleteTrip(trip._id);
-  };
-
   return (
     <View
       style={{
@@ -26,44 +17,24 @@ const TripItem = ({ trip, navigation }) => {
         borderRadius: 10,
       }}
     >
-
-      <HStack w="100%" alignItems="center" space="3">
-        {trip.image && (
-          <Image
-            source={{
-              uri: baseUrl + trip.image,
-            }}
-            alt="image"
-            style={{ width: 100, height: 100 }}
-          />
-        )}
-        <Text>{trip.title}</Text>
-      </HStack>
-
-      {authStore.user?._id === trip.owner && (
-        <Button onPress={handleDelete}>Delete Trip</Button>
-      )}
-      {/* {authStore.user?._id === trip.owner && (
-        <Button onPress={navigation.navigate('UpdateTrip')}>update Trip</Button>
-      )}
-      {authStore.user?._id === trip.owner && (
-        <Button
-          onPress={
-            (() => navigation.navigate("UpdateTrip"), { trip: trip })
-          }
-        >
-          update Trip
-        </Button>
-      )} */}
+      <Pressable
+        onPress={() => navigation.navigate("TripDetail", { trip: trip })}
+      >
+        <HStack w="100%" alignItems="center" space="3">
+          {trip.image && (
+            <Image
+              source={{
+                uri: baseUrl + trip.image,
+              }}
+              alt="image"
+              style={{ width: 100, height: 100 }}
+            />
+          )}
+          <Text>{trip.title}</Text>
+        </HStack>
+      </Pressable>
     </View>
-
   );
 };
 
-export default observer(TripItem);
-// maybe we can use an icon instead of a button for deleting?
-// //  {authStore.user
-// ? authStore.user._id === trip.owner && (
-//   <Button onPress={handleDelete}>Delete Trip</Button>
-// )
-// : null}
+export default TripItem;
--- components/TripList/UpdateTrip.js
+++ components/TripList/UpdateTrip.js
@@ -1,38 +1,42 @@
-
 import { Button, FormControl, Input } from "native-base";
 import React, { useState } from "react";
-import { View } from "react-native";
+import { Text, View } from "react-native";
+
 import tripStore from "../../stores/tripStore";
 
 // i'm still working on this, i'm taking a break- will continue later.
-const UpdateTrip = (props) => {
-  const [trip, setTrip] = useState({
-    title: "",
-    image: "",
-    description: "",
+const UpdateTrip = ({ route, navigation }) => {
+  const { trip } = route.params;
+  const [_trip, setTrip] = useState({
+    title: trip.title,
+    image: trip.image,
+    description: trip.description,
   });
 
-  const handleUpdate = async () => tripStore.updateTrip(props.trip._id, trip);
+  const handleUpdate = async () => {
+    tripStore.updateTrip(trip._id, _trip);
+    navigation.goBack("TripDetail", { trip: _trip });
+  };
   return (
     <View>
       <FormControl>
         <FormControl.Label>updated Trip Name:</FormControl.Label>
-        <Input onChangeText={(title) => setTrip({ ...trip, title })} />
+
+        <Input onChangeText={(title) => setTrip({ ..._trip, title })} />
       </FormControl>
       <FormControl>
         <FormControl.Label>updated Image:</FormControl.Label>
-        <Input onChangeText={(image) => setTrip({ ...trip, image })} />
+        <Input onChangeText={(image) => setTrip({ ..._trip, image })} />
       </FormControl>
       <FormControl>
         <FormControl.Label>updated Description:</FormControl.Label>
         <Input
-          onChangeText={(description) => setTrip({ ...trip, description })}
+          onChangeText={(description) => setTrip({ ..._trip, description })}
         />
-        <Button mt="2" colorScheme="indigo" onPress={handleUpdate}>
-          update Trip
-        </Button>
       </FormControl>
-
+      <Button mt="2" colorScheme="indigo" onPress={handleUpdate}>
+        update Trip
+      </Button>
     </View>
   );
 };
