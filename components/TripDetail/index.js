b90f9a11d542864234375ead681cfa876e28bbf3
Alkandari-Y <yayalkandari@gmail.com>
2021-11-25 13:19:25 +0300

    profile sync

--- components/TripDetail/index.js
+++ components/TripDetail/index.js
@@ -6,9 +6,14 @@ import tripStore from "../../stores/tripStore";
 import { baseUrl } from "../../stores/baseUrl";
 import authStore from "../../stores/authStore";
 import styles from "./styles";
+import profileStore from "../..//stores/profileStore";
 
 const TripDetail = ({ route, navigation }) => {
   const { trip } = route.params;
+
+  const userProfile = profileStore.profiles.find(profile => 
+    profile._id === trip.owner.profile
+  );
 
   if (tripStore.isLoading) {
     return <Spinner />;
@@ -17,8 +22,8 @@ const TripDetail = ({ route, navigation }) => {
     tripStore.deleteTrip(trip._id);
     navigation.navigate("TripList");
   };
-  return (
 
+  return (
     <View style={styles.background}>
       <Image
         style={styles.Image}
@@ -26,9 +31,9 @@ const TripDetail = ({ route, navigation }) => {
         alt="image"
       />
       <Box style={styles.profileContainer}>
-        <Box style={styles.profilePic}>Profile Picture</Box>
+        <Box style={styles.profilePic} source={{uri: baseUrl + userProfile}}>Profile Picture</Box>
         <Box style={styles.buttons}>
-          {authStore.user?._id === trip.owner && (
+          {authStore.user?._id === trip.owner._id && (
             <>
               <Button
                 onPress={() =>
